5b5f82eecdb2e3dc0a32e55c737df517f9a06300
aKshad <gawdeakshad@gmail.com>
2024-05-10 22:13:55 +0530

    Delete Book

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
+import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox } from "react-icons/md";
 
@@ -71,6 +71,9 @@ const Home = () => {
                     </Link>
                     <Link to={`/books/edit/${book._id}`}>
                       <AiOutlineEdit className="text-blue-800 text-2xl" />
+                    </Link>
+                    <Link to={`/books/delete/${book._id}`}>
+                      <AiOutlineDelete className="text-red-800 text-2xl" />
                     </Link>
                   </div>
                 </td>
